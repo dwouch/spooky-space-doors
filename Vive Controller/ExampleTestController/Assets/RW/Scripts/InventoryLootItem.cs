7ac4b3ba90b16891a12b6bab802e983e41833c70
David Wouch <dwouch@gmail.com>
2019-06-05 21:29:58 -0500

    Skeletal code for item superclass, droppable interface,& item controller

---
+++ Vive Controller/ExampleTestController/Assets/RW/Scripts/InventoryLootItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is a parent class for all items that can be dropped or stored in inventory
+public class InventoryLootItem : MonoBehaviour
+{
+    //Reference ID for item drop logic to refer back to
+    public int refID;
+    //The name of the item
+    public string itemName;
+    //The description for the item
+    public string itemDescription;
+    //Indicates whether the item is a unique type or not
+    public bool isUniqueType;
+    //Rigidbody
+    private Rigidbody obj;
+
+
+    public void onCreate() { }
+
+    public void use() { }
+
+    public void onDestroy() { }
+
+    public int getRefID()
+    {
+        return refID;
+    }
+
+    public string getName()
+    {
+        return itemName;
+    }
+
+    public string getDescription()
+    {
+        return itemDescription;
+    }
+
+    public bool isUnique()
+    {
+        return isUniqueType;
+    }
+
+}
---
+++ Vive Controller/ExampleTestController/Assets/RW/Scripts/ItemController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public Dictionary<int, InventoryLootItem> commonItems;
+    public Dictionary<int, InventoryLootItem> uniqueItems;
+
+    public ItemController() { }
+    public int dropItem()
+    {
+        return 0;
+    }
+}
